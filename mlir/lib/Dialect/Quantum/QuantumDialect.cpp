6c4c91026943a04a8815fe75fd9ff13296d1aa88
Naoki Shibuya <naokishibuya@me.com>
2025-07-16 09:08:35 +0900

    update to avoid duplicated definitions

--- mlir/lib/Dialect/Quantum/QuantumDialect.cpp
+++ mlir/lib/Dialect/Quantum/QuantumDialect.cpp
@@ -3,11 +3,4 @@
 using namespace mlir;
 using namespace mlir::quantum;
 
-#include "mlir/Dialect/Quantum/IR/QuantumOpsDialect.cpp.inc"
-
-void QuantumDialect::initialize() {
-  addOperations<
-#define GET_OP_LIST
-#include "mlir/Dialect/Quantum/IR/QuantumOps.cpp.inc"
-  >();
-}
+// The dialect definition is included in QuantumOps.cpp to avoid multiple definitions
