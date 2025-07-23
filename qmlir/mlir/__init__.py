5226f385cf65703f75d5c47ce79456426a2c6cc1
Naoki Shibuya <naokishibuya@me.com>
2025-07-23 09:20:14 +0900

    Update remove get_quantum_opt_path from module export

--- qmlir/mlir/__init__.py
+++ qmlir/mlir/__init__.py
@@ -5,6 +5,6 @@ circuit transpilation to MLIR and optimization tools.
 """
 
 from .transpiler import circuit_to_mlir, apply_passes
-from .config import get_quantum_opt_path
 
-__all__ = ["circuit_to_mlir", "apply_passes", "get_quantum_opt_path"]
+
+__all__ = ["circuit_to_mlir", "apply_passes"]
